45cc1325a365e96964f3d8cd1387bbeff896551e
Kronyx67 <juju.emanuel67@gmail.com>
2025-10-23 15:36:07 +0200

    multi page + renomme data

---
+++ streamlit/Solo.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_page():
+    st.title("Bienvenue sur l'Accueil 🏠")
+    st.write("Ceci est le contenu de la page d'accueil.")
--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,22 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
-page = st_navbar(["Accueil", "Solo", "Duo"])
+import Accueil
+import Solo
+import Duo
 
+st.set_page_config(page_title="Mon App Multi-Pages", layout="wide")
+
+
+
+pages = st_navbar(["Accueil", "Solo", "Duo"])
+
+
+
+# Affichage de la page sélectionnée
+if pages == "Accueil":
+    Accueil.show_page()
+elif pages == "Solo":
+    Solo.show_page()
+elif pages == "Duo":
+    Duo.show_page()
