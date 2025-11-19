7d925c00d76444123fbc4b8fc538910747eb8620
Kronyx67 <juju.emanuel67@gmail.com>
2025-11-19 14:05:08 +0100

    cache des data mis dans le main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
+import os
+import pandas as pd
 
 import Accueil
 import Solo
@@ -7,7 +9,18 @@ import Duo
 
 st.set_page_config(page_title="Mon App Multi-Pages", layout="wide")
 
+# --- 1. Chargement optimisé des CSV une seule fois ---
+@st.cache_data
+def load_csv_folder(folder):
+    dfs = {}
+    for file in os.listdir(folder):
+        if file.endswith(".csv"):
+            dfs[file] = pd.read_csv(os.path.join(folder, file))
+    return dfs
 
+# Initialisation dans session_state (une seule fois par session)
+if "data" not in st.session_state:
+    st.session_state.data = load_csv_folder("data")
 
 pages = st_navbar(["Accueil", "Solo", "Duo"])
 
